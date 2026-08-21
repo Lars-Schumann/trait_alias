0a134857bde7820f73cc95e09e4973f30c086866
Lars Schumann <LarsSchumann@mail.de>
2026-08-21 05:26:32 +0200

    feat: no_std

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
+#![no_std]
+
 #[macro_export]
 macro_rules! trait_alias {
     ($vis:vis trait $alias:ident$(<$($generics:ident),*>)? = $($super_traits:tt)+) => {
-
         $vis trait $alias$(<$($generics),*>)?: $($super_traits)+ {}
-
         impl<__ඞඞT: $($super_traits)+, $($($generics),*)?> $alias$(<$($generics),*>)? for __ඞඞT {}
     };
 }
