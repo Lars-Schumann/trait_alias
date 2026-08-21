c3b7086763f721d07f4a199b1ef21af668d6b409
Lars Schumann <LarsSchumann@mail.de>
2026-08-21 05:25:41 +0200

    refactor: simplify naming

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #[macro_export]
 macro_rules! trait_alias {
-    ($vis:vis trait $alias_name:ident$(<$($generics:ident),*>)? = $($super_traits:tt)+) => {
+    ($vis:vis trait $alias:ident$(<$($generics:ident),*>)? = $($super_traits:tt)+) => {
 
-        $vis trait $alias_name$(<$($generics),*>)?: $($super_traits)+ {}
+        $vis trait $alias$(<$($generics),*>)?: $($super_traits)+ {}
 
-        impl<__ඞඞT: $($super_traits)+, $($($generics),*)?> $alias_name$(<$($generics),*>)? for __ඞඞT {}
+        impl<__ඞඞT: $($super_traits)+, $($($generics),*)?> $alias$(<$($generics),*>)? for __ඞඞT {}
     };
 }
